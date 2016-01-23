5f61ecec4303b076aca6609294e36159c459b7c0
Xavi S.B <xavi@typeform.com>
2016-01-23 15:15:33 +0100

    remove json annotations

--- insightapi/types.go
+++ insightapi/types.go
@@ -3,74 +3,74 @@ package insightapi
 // Go implementation of the Insight API https://insight.is
 
 type Block struct {
-	Hash          string   `json:"hash"`
-	Confirmations int      `json:"confirmations"`
-	Size          int      `json:"size"`
-	Height        int      `json:"height"`
-	Version       int      `json:"version"`
-	Merkleroot    string   `json:"merkleroot"`
-	Tx            []string `json:"tx:omitempty"`
-	Time          int      `json:"time"`
-	Nonce         int      `json:"nonce"`
-	Bits          string   `json:"bits"`
-	Difficulty    int      `json:"difficulty"`
-	Chainwork     string   `json:"chainwork"`
-	Nextblockhash string   `json:"nextblockhash"`
-	IsMainChain   bool     `json:"isMainChain"`
+	Hash          string
+	Confirmations int
+	Size          int
+	Height        int
+	Version       int
+	Merkleroot    string
+	Tx            []string
+	Time          int
+	Nonce         int
+	Bits          string
+	Difficulty    int
+	Chainwork     string
+	Nextblockhash string
+	IsMainChain   bool
 }
 
 type Tx struct {
-	Txid     string   `json:"txid"`
-	Version  int      `json:"version"`
-	Locktime int      `json:"locktime"`
-	Vin      []Input  `json:"vin"`
-	Vout     []Output `json:"vout"`
-	ValueOut float32  `json:"valueOut"`
-	Size     int      `json:"size"`
-	ValueIn  float32  `json:"valueIn"`
-	Fees     float32  `json:"fees"`
+	Txid     string
+	Version  int
+	Locktime int
+	Vin      []Input
+	Vout     []Output
+	ValueOut float32
+	Size     int
+	ValueIn  float32
+	Fees     float32
 }
 
 type Input struct {
-	Txid             string   `json:"txid"`
-	Vout             int      `json:"vout"`
-	ScriptSig        []string `json:"scriptSig"`
-	Sequence         int      `json:"sequence"`
-	N                int      `json:"n"`
-	Addr             string   `json:"addr"`
-	ValueSat         float32  `json:"valueSat"`
-	Value            float32  `json:"value"`
-	DoubleSpentTxID  string   `json:"doubleSpentTxID"`
-	IsConfirmed      bool     `json:"isConfirmed"`
-	Confirmations    int      `json:"confirmations"`
-	UnconfirmedInput bool     `json:"unconfirmedInput"`
+	Txid             string
+	Vout             int
+	ScriptSig        []string
+	Sequence         int
+	N                int
+	Addr             string
+	ValueSat         float32
+	Value            float32
+	DoubleSpentTxID  string
+	IsConfirmed      bool
+	Confirmations    int
+	UnconfirmedInput bool
 }
 
 type Output struct {
-	Value        string       `json:"value"`
-	N            int          `json:"n"`
-	ScriptPubKey ScriptPubKey `json:"scriptPubKey"`
+	Value        string
+	N            int
+	ScriptPubKey ScriptPubKey
 }
 
 type ScriptPubKey struct {
-	Asm       string   `json:"asm"`
-	Hex       string   `json:"hex"`
-	ReqSigs   int      `json:"reqSigs"`
-	Type      string   `json:"type"`
-	Addresses []string `json:"addresses"`
+	Asm       string
+	Hex       string
+	ReqSigs   int
+	Type      string
+	Addresses []string
 }
 
 type Addr struct {
-	AddrStr                 string   `json:"addrStr"`
-	Balance                 float32  `json:"balance"`
-	BalanceSat              float32  `json:"balanceSat"`
-	TotalReceived           float32  `json:"totalReceived"`
-	TotalReceivedSat        float32  `json:"totalReceivedSat"`
-	TotalSent               float32  `json:"totalSent"`
-	TotalSentSat            float32  `json:"totalSentSat"`
-	UnconfirmedBalance      int      `json:"unconfirmedBalance"`
-	UnconfirmedBalanceSat   int      `json:"unconfirmedBalanceSat"`
-	UnconfirmedTxApperances int      `json:"unconfirmedTxApperances"`
-	TxApperances            int      `json:"txApperances"`
-	Transactions            []string `json:"transactions,omitempty"`
+	AddrStr                 string
+	Balance                 float32
+	BalanceSat              float32
+	TotalReceived           float32
+	TotalReceivedSat        float32
+	TotalSent               float32
+	TotalSentSat            float32
+	UnconfirmedBalance      int
+	UnconfirmedBalanceSat   int
+	UnconfirmedTxApperances int
+	TxApperances            int
+	Transactions            []string
 }
