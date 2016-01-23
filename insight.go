c0d529554c19cd17e9c9581b640a31d2b41dd5c1
Xavi S.B <xavi@typeform.com>
2016-01-23 12:34:34 +0100

    add addr type

--- insight.go
+++ insight.go
@@ -3,20 +3,20 @@ package insight
 // Go implementation of the Insight API https://insight.is
 
 type Block struct {
-	Hash          string      `json:"hash"`
-	Confirmations int         `json:"confirmations"`
-	Size          int         `json:"size"`
-	Height        int         `json:"height"`
-	Version       int         `json:"version"`
-	Merkleroot    string      `json:"merkleroot"`
-	Tx            []InsightTx `json:"tx:omitempty"`
-	Time          int         `json:"time"`
-	Nonce         int         `json:"nonce"`
-	Bits          string      `json:"bits"`
-	Difficulty    int         `json:"difficulty"`
-	Chainwork     string      `json:"chainwork"`
-	Nextblockhash string      `json:"nextblockhash"`
-	IsMainChain   bool        `json:"isMainChain"`
+	Hash          string `json:"hash"`
+	Confirmations int    `json:"confirmations"`
+	Size          int    `json:"size"`
+	Height        int    `json:"height"`
+	Version       int    `json:"version"`
+	Merkleroot    string `json:"merkleroot"`
+	Tx            []Tx   `json:"tx:omitempty"`
+	Time          int    `json:"time"`
+	Nonce         int    `json:"nonce"`
+	Bits          string `json:"bits"`
+	Difficulty    int    `json:"difficulty"`
+	Chainwork     string `json:"chainwork"`
+	Nextblockhash string `json:"nextblockhash"`
+	IsMainChain   bool   `json:"isMainChain"`
 }
 
 type Tx struct {
@@ -40,7 +40,7 @@ type Input struct {
 	Addr             string   `json:"addr"`
 	ValueSat         float32  `json:"valueSat"`
 	Value            float32  `json:"value"`
-	DoubleSpentTxID  nil      `json:"doubleSpentTxID"`
+	DoubleSpentTxID  string   `json:"doubleSpentTxID"`
 	IsConfirmed      bool     `json:"isConfirmed"`
 	Confirmations    int      `json:"confirmations"`
 	UnconfirmedInput bool     `json:"unconfirmedInput"`
@@ -58,4 +58,19 @@ type ScriptPubKey struct {
 	ReqSigs   int      `json:"reqSigs"`
 	Type      string   `json:"type"`
 	Addresses []string `json:"addresses"`
+}
+
+type Addr struct {
+	AddrStr                 string   `json:"addrStr"`
+	Balance                 float32  `json:"balance"`
+	BalanceSat              float32  `json:"balanceSat"`
+	TotalReceived           float32  `json:"totalReceived"`
+	TotalReceivedSat        float32  `json:"totalReceivedSat"`
+	TotalSent               float32  `json:"totalSent"`
+	TotalSentSat            float32  `json:"totalSentSat"`
+	UnconfirmedBalance      int      `json:"unconfirmedBalance"`
+	UnconfirmedBalanceSat   int      `json:"unconfirmedBalanceSat"`
+	UnconfirmedTxApperances int      `json:"unconfirmedTxApperances"`
+	TxApperances            int      `json:"txApperances"`
+	Transactions            []string `json:"transactions,omitempty"`
 }
